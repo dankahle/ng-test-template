400003f68a4bc45fe358e535ef6b5485343053b2
Dan Kahle <dan.kahle@gmail.com>
2018-01-12 14:44:10 -0800

    intial

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CoreModule} from '../core/core.module';
 import {FormsModule} from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreModule,
-    FormsModule
+    FormsModule,
+    FlexLayoutModule
   ],
   exports: [
     CommonModule,
     CoreModule,
-    FormsModule
+    FormsModule,
+    FlexLayoutModule
   ],
   declarations: []
 })
